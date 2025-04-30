19e29c6306741120fb474ae924584d1bac60aea9
wirawanmahardika <wiraprogrammer@gmail.com>
2025-05-01 05:58:51 +0800

    feat: menggunakan whatsapp untuk contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,17 +1,20 @@
 export default function Contact() {
+    const directToWA = (e: any) => {
+        e.preventDefault()
+
+        const noWa = import.meta.env.VITE_WA
+        const message = e.target.message.value
+        const anchorTag = document.createElement("a")        
+        anchorTag.href = `https://wa.me/${noWa}?text=${message}`
+        anchorTag.target = "_blank"
+        anchorTag.click()
+    }
+
     return <section id="contact" className="mt-10">
         <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
 
-            <form action="#" className="space-y-8">
-                <div>
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-                    <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="test@example.com" required />
-                </div>
-                <div>
-                    <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-                    <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required />
-                </div>
+            <form onSubmit={directToWA} className="space-y-8">
                 <div className="sm:col-span-2">
                     <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
                     <textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
